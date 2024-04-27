71bc05ff2836584cf206b99d8c27363fb05bfa4e
lizuAg <wndms058@gmail.com>
2024-04-27 16:37:01 +0900

    refactor: optimize imports

--- src/main/java/app/iris/missingyou/controller/MemberController.java
+++ src/main/java/app/iris/missingyou/controller/MemberController.java
@@ -1,25 +1,21 @@
 package app.iris.missingyou.controller;
 
 import app.iris.missingyou.dto.MemberInfoDto;
-import app.iris.missingyou.dto.PostCreateResponseDto;
 import app.iris.missingyou.dto.PushInfoDto;
-import app.iris.missingyou.exception.ErrorResponse;
 import app.iris.missingyou.security.CustomUserDetails;
-import app.iris.missingyou.service.MemberService;
 import app.iris.missingyou.service.FCMService;
+import app.iris.missingyou.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.SchemaProperties;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
