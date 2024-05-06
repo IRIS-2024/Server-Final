d1f1c051df09e27fc87df0372aa169f8c7543187
lizuAg <wndms058@gmail.com>
2024-05-06 12:30:59 +0900

    feat: 디바이스 토큰 쿼리 추가

--- src/main/java/app/iris/missingyou/repository/PushRepository.java
+++ src/main/java/app/iris/missingyou/repository/PushRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,8 +15,13 @@ import java.util.Optional;
 public interface PushRepository extends JpaRepository<Push, Long> {
     Optional<Push> findByMemberId(Long memberId);
 
+    @Query("SELECT DISTINCT push.deviceToken FROM Push push " +
+            "INNER JOIN Post post ON push.member.id = post.author.id " +
+            "WHERE post.id = :pid ")
+    Optional<String> findPostAuthorDeviceToken(@Param("pid") Long pid);
+
     @Query("SELECT p.deviceToken FROM Push p "
-            +"WHERE p.region = :region AND p.deviceToken IS NOT NULL "
+            +"WHERE p.region = :region AND p.deviceToken IS NOT NULL AND p.member.id <> :memberId "
             + "ORDER BY p.updatedAt DESC")
-    Page<String> findAllDeviceToken(Region region, Pageable pageable);
+    Page<String> findAllDeviceToken(@Param("region") Region region, @Param("memberId") Long memberId, Pageable pageable);
 }
