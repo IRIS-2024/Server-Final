e47d8e696aa168f5b2b59254005e08c6092bee06
lizuAg <wndms058@gmail.com>
2024-05-06 12:23:18 +0900

    refactor: token exception log 개선

--- src/main/java/app/iris/missingyou/security/jwt/TokenValidator.java
+++ src/main/java/app/iris/missingyou/security/jwt/TokenValidator.java
@@ -18,11 +18,15 @@ public class TokenValidator {
     public boolean verifyToken(String token) {
         try{
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-        }catch (ExpiredJwtException expiredJwtException){
+        } catch (ExpiredJwtException e) {
+            log.warn("만료된 토큰: (만료 시간: " + e.getClaims().getExpiration().toString() + ")");
             throw new CustomException(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다.");
-        }catch (Exception e){
-            log.error("[in validateToken] refreshToken: {}", token);
+        } catch (IllegalArgumentException | MalformedJwtException | SecurityException e) {
+            log.warn("Token: {}", token);
             throw new CustomException(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.");
+        } catch (Exception e) {
+            log.error("Unexpected error: {}", e.getMessage());
+            throw new CustomException(HttpStatus.UNAUTHORIZED, "알 수 없는 오류");
         }
         return true;
     }
